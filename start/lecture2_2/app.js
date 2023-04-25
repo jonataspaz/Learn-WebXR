c2991dcac0af9d16a5588f76edd35aad42de0b63
Jonatas Paz <jopaz9@gmail.com>
2023-04-26 09:32:14 +1200

    update lecture 2_3 earth moon

--- start/lecture2_2/app.js
+++ start/lecture2_2/app.js
@@ -10,26 +10,35 @@ class App {
       60,
       window.innerWidth / window.innerHeight,
       0.1,
-      1000
+      100
     );
     this.camera.position.set(0, 0, 4);
 
     this.scene = new THREE.Scene();
-    this.scene.background = new THREE.Color(0x457ede);
+    this.scene.background = new THREE.Color(0xaaaaaa);
 
-    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    const ambient = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 0.3);
+    this.scene.add(ambient);
+
+    const light = new THREE.DirectionalLight();
+    light.position.set(0.2, 1, 1);
+    this.scene.add(light);
+
+    this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
     this.renderer.setPixelRatio(window.devicePixelRatio);
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     container.appendChild(this.renderer.domElement);
 
-    this.renderer.setAnimationLoop(this.render.bind(this));
-
     const geometry = new THREE.BoxBufferGeometry();
-    const material = new THREE.MeshBasicMaterial({ color: 0xaaaaaa });
+    const material = new THREE.MeshStandardMaterial({ color: 0xff0000 });
 
     this.mesh = new THREE.Mesh(geometry, material);
 
     this.scene.add(this.mesh);
+
+    const controls = new OrbitControls(this.camera, this.renderer.domElement);
+
+    this.renderer.setAnimationLoop(this.render.bind(this));
 
     window.addEventListener("resize", this.resize.bind(this));
   }
@@ -41,6 +50,7 @@ class App {
   }
 
   render() {
+    this.mesh.rotateX(0.01);
     this.mesh.rotateY(0.01);
     this.renderer.render(this.scene, this.camera);
   }
